7b6295d898a2dcf1e4f30ca646158fd981a19d5d
Syed Zaman Mostafiz <zamancsediue44@gmail.com>
2017-07-24 00:03:38 -0700

    Working Stage is updated

---
+++ WorkingStage/My02nd Project ( University’s Data Management System )/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>www.webcoachbd.com</title>
+	<link rel="stylesheet" href="css/style.css" />
+	
+</head>
+<body>
+	<div class="menubar">
+		<ul class="header_menu">
+			<li><a href="index.php">Home</a></li>
+			<li class="dropdown">
+				<a href="index.php">Register</a>
+				<ul class="drop">
+					<li><a href="TeachersReg.php">Teachers Register</a></li>
+					<li><a href="studentsReg.php">Students Register</a></li>
+				</ul>
+			</li>
+			<li class="dropdown">
+				<a href="">Login</a>
+				<ul class="drop">
+					<li><a href="Admin.php" target="_blank">Admin Login</a></li>
+					<li><a href="Teachers.php">Teachers Login</a></li>
+					<li><a href="students.php">Students Login</a></li>
+				</ul
+			</li>
+			<li><a><input type="text" name="search" placeholder="Search.."></a></li>
+			
+			<li><a href="Contact.html">Contact</a></li>
+		</ul>
+		</div>
+		<div class="content">
+		
+		</div>
+		<footer></footer>
+</body>
+</html>
---
+++ WorkingStage/TrainingWithLiveProject/01.LiveWebsiteDesignTutorials/Part-08/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Our Project.</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="headersection tamplate clear">
+		<h2>Dhaka International University.</h2>
+	</div>
+	<div class="navsection tamplate clear">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#">Privacy</a></li>
+			<li><a href="register.php">Register</a></li>
+			<li><a href="#">Login</a></li>
+		</ul>
+	</div>
