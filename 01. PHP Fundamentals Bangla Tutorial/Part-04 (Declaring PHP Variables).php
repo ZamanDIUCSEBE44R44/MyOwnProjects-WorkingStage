54b2bf57290ab0ae6cac4fc2949107805b124ecd
Syed Zaman Mostafiz <zamancsediue44@gmail.com>
2017-10-31 13:54:39 +0600

    PHP Fundamentals Part-11

---
+++ 01. PHP Fundamentals Bangla Tutorial/Part-04 (Declaring PHP Variables).php
@@ -0,0 +1,42 @@
+<?php 
+$font = "verdana";
+$bgcolor = "#444";//E06C1D
+$fontcolor = "#E06C1D"; 
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Syntax</title>
+	<style type="text/css">
+	body{font-family:<?php echo $font;?>}
+	.phpcoding{width:900px; margin:0 auto; background:<?php echo "#ddd";?>; }
+	.headeroption, .footeroption{background:<?php echo $bgcolor;?>; color:<?php echo $fontcolor;?>;text-align:center; padding:20px;}
+	.headeroption h2, .footeroption h2{margin:0px}
+	.maincontent{min-height:400px; padding:20px}
+	</style>
+</head>
+<body>
+	<div class="phpcoding">
+		<section class="headeroption">
+			<h2><?php echo "PHP Fundamental Training";?></h2>
+		</section>
+		<section class="maincontent">
+		<?php
+			echo $font;
+			$a = 5;
+			$b= 10;
+			$c= $a+$b;
+			echo "<br />Total Value is  ".$c."OK Fine";
+		?>
+		<br /><a href="Part-05 (Show Output in PHP).php">Part-05 (Show Output in PHP)</a>
+
+		</section>
+		<section class="footeroption">
+			<h2><?php echo "www.trainingwithliveproject.com";?></h2>
+		</section>
+		
+	</div>
+	
+</body>
+</html>
---
+++ 01. PHP Fundamentals Bangla Tutorial/Project/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Syntax</title>
+</head>
+<body>
+<section class="phpcoding">
+<?php
+echo "I Love PHP";
+?>	
+</section>
+</body>
+</html>
